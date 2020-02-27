3b119c83700a52a36260f69126a09acf54469be4
Zach Mommaerts <zmommaerts@gmail.com>
2020-02-26 22:16:40 -0600

    js updated to show only the rolls of the user player

--- diceGame.js
+++ diceGame.js
@@ -1,46 +1,64 @@
 "use strict";
+var playerOne = {score : 0}
+var playerTwo = {score : 0}
+var playerThree = {score : 0}
+var playerFour = {score : 0}
+var playerFive = {score : 0}
+var playerSix = {score : 0}
+var playerSeven = {score : 0}
+var playerEight = {score : 0}
+var playerNine = {score : 0}
+var playerTen = {score : 0}
 
 var players = [{playerOne},{playerTwo},{playerThree},{playerFour},{playerFive},{playerSix},{playerSeven},{playerEight},{playerNine},{playerTen}]
 
 function rollDice(){
-    let diceRolls = [Math.floor((Math.random() * 4) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 8) + 1), Math.floor((Math.random() * 10) + 1),Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 20) + 1)].reduce((a, b) => a + b, 0)
-    return diceRolls;
+    let diceRolls = [Math.floor((Math.random() * 4) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 8) + 1), Math.floor((Math.random() * 10) + 1),Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 20) + 1)]
+    if(document.getElementById("4").innerHTML == 0){
+        document.getElementById("4").innerHTML = diceRolls[0];
+        document.getElementById("6").innerHTML = diceRolls[1];
+        document.getElementById("8").innerHTML = diceRolls[2];
+        document.getElementById("10").innerHTML = diceRolls[3];
+        document.getElementById("12").innerHTML = diceRolls[4];
+        document.getElementById("20").innerHTML = diceRolls[5];
+    }
+    let sumOfRolls = diceRolls.reduce((a, b) => a + b, 0)
+    return sumOfRolls;
 }
 function rollTwenty(){
     let shootOutRolls = [Math.floor((Math.random() * 20) + 1),Math.floor((Math.random() * 20) + 1),Math.floor((Math.random() * 20) + 1),Math.floor((Math.random() * 20) + 1)];
     let finalRoll = shootOutRolls[Math.floor(Math.random() * shootOutRolls.length)]
     return finalRoll;
 }
-function tieBreaker(){
-
-}
 function commenceRound(){
     if(players.length > 4){
         let i;
         for (i = 0; i < players.length; i++){
-            players[i] = rollDice();
+            players[i].score = rollDice();
         }
         removePlayer();
         removePlayer();
-        break;
     }
     if(players.length <= 4 && players.length > 2){
         let i;
         for (i = 0; i < players.length; i++){
-            players[i] = rollDice();
+            players[i].score = rollDice();
         }
         removePlayer();
-        break;
     }
     else{
         let i;
         for (i = 0; i < players.length; i++){
-            players[i] = rollTwenty();
+            players[i].score = rollTwenty();
             removePlayer();
         }
     }
 }
 function removePlayer(){
     let lowestPlayer = Math.min(players);
-    playerOusted = players.splice(players.indexOf(lowestPlayer));
+    players = players.splice(players.indexOf(lowestPlayer));
+    return players;
+}
+function runGame(){
+    commenceRound();
 }
